b3f9840eb83077b8f9a11707a1b5e8cf5cf90166
filipempribeiro-sys <filipe.m.p.ribeiro@gmail.com>
2026-09-04 14:40:15 +0100

    Atualizar o sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME="project-alpha-v2.2.1-route-visible-arrow";
+const CACHE_NAME="project-alpha-v2.2.2-svg-route-overlay";
 const SHELL=["./","./index.html","./manifest.webmanifest","./icon-192.png","./icon-512.png"];
 self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(SHELL).catch(()=>{})))});
 self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
