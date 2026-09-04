1f8d01af8dd6183295b4efa06437be2f540e0954
filipempribeiro-sys <filipe.m.p.ribeiro@gmail.com>
2026-09-04 14:13:27 +0100

    Atualizar o sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME="project-alpha-v2.1.1-navigation-stability-fix";
+const CACHE_NAME="project-alpha-v2.2.0-real-nav-smart-chat";
 const SHELL=["./","./index.html","./manifest.webmanifest","./icon-192.png","./icon-512.png"];
 self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(SHELL).catch(()=>{})))});
 self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
