7ff51149ce36fb13553eaca14e5cb09be17440a0
filipempribeiro-sys <filipe.m.p.ribeiro@gmail.com>
2026-09-04 08:15:20 +0100

    Atualizar o sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME="project-alpha-v1.4.15-version-silver-fix";
+const CACHE_NAME="project-alpha-v1.4.16-gold-explore";
 const SHELL=["./","./index.html","./manifest.webmanifest","./icon-192.png","./icon-512.png"];
 self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(SHELL).catch(()=>{})))});
 self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
