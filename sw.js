37c3e6965e6a67d30f9c962c680133a45831c1bc
filipempribeiro-sys <filipe.m.p.ribeiro@gmail.com>
2026-09-09 18:02:20 +0100

    Atualizar o sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME="project-alpha-v4.1.0-deep-research-spotify-share";
+const CACHE_NAME="project-alpha-v4.2.0-global-spotify-remote";
 const SHELL=["./","./index.html","./manifest.webmanifest","./icon-192.png","./icon-512.png"];
 self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(SHELL).catch(()=>{})))});
 self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
