a33eb7bdc87f34d8a838ac9c1739e3cf8d4f5c44
filipempribeiro-sys <filipe.m.p.ribeiro@gmail.com>
2026-09-09 18:37:43 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME="project-alpha-v4.2.0-global-spotify-remote";
+const CACHE_NAME="project-alpha-v4.2.1-global-spotify-remote-premium-nav-fix";
 const SHELL=["./","./index.html","./manifest.webmanifest","./icon-192.png","./icon-512.png"];
 self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(SHELL).catch(()=>{})))});
 self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
